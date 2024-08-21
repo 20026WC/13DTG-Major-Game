6df5ebec1d424e2bd92ff5130fb2e145b8da0752
20026WC <20026@wc.school.nz>
2024-08-21 16:56:59 +1200

    Got an animation working.
    - Got animation finally working.

--- Assets/Scripts/HitWeakness.cs
+++ Assets/Scripts/HitWeakness.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class HitWeakness : MonoBehaviour
 {
-    public bool WeaknessWasHit = false;
+    public bool WeaknessWasHit;
+
     Rigidbody2D enemyRb;
     public Animator animator;
     // Start is called before the first frame update
@@ -17,7 +18,8 @@ public class HitWeakness : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,16 +27,25 @@ public class HitWeakness : MonoBehaviour
         if (other.gameObject.CompareTag("Weapon"))
         {
             animator.SetBool("WeaknessHit", true);
-            // Gets destroyed if it touches the player's weapon.
-            PlayerHitWeakness();
+            
 
         }
     }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            PlayerHitWeakness();
+            animator.SetBool("WeaknessHit", false);
+
+        }
+    }
     IEnumerator PlayerHitWeakness()
     {
-        
-        yield return new WaitForSeconds(5);
-        animator.SetBool("WeaknessHit", false);
+        WeaknessWasHit = true;
+        yield return new WaitForSeconds(10);
+        WeaknessWasHit = false;
     }
 }
